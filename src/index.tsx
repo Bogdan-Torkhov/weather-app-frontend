ed24969e02227b2fcdc0e13bdb0a05201efbd6fd
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-06-09 15:48:11 +0300

    Created reset styles component
    added types for styled-components
    created globalStyles component with reset styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import { Router } from './Router/appRouter';
 import { BrowserRouter } from 'react-router-dom';
+import { ResetStyles } from 'components/stylesReset'
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
+    <ResetStyles />
     <Router />
   </BrowserRouter>
 );
