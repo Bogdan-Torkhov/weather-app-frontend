91d90d0546ce08551089b350745ef3a1b088a22a
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-06-09 17:27:18 +0300

    Added fake api request

--- src/pages/landingPage/index.tsx
+++ src/pages/landingPage/index.tsx
@@ -4,12 +4,32 @@ import { PageBasicLayout, Header, HeaderTitle, PageContentBlock, WeatherPreview,
 // TODO: Разбить компонент на подкомпоненты.
 
 export const LandingPage = () => {
-    const [currentCity, setCurrentCity] = useState('Paris')
-    const [currentTemperature, setCurrentTemperature] = useState(0)
-    const [currentPresure, setCurrentPresure] = useState(0)
-    const [currentWindSpeed, setCurrentWindSpeed] = useState(0)
-    const [currentUVIndex, setCurrentUVIndex] = useState(0)
-    const [currentPrecipitation, setCurrentPrecipitation] = useState(0)
+    const [currentCity, setCurrentCity] = useState<string>('Paris')
+    const [currentTemperature, setCurrentTemperature] = useState<number>(0)
+    const [currentPresure, setCurrentPresure] = useState<number>(0)
+    const [currentWindSpeed, setCurrentWindSpeed] = useState<number>(0)
+    const [currentUVIndex, setCurrentUVIndex] = useState<number>(0)
+    const [currentPrecipitation, setCurrentPrecipitation] = useState<number>(0)
+
+    function changeCurrentCity(city: string){
+        setCurrentCity(city)
+    }
+
+    function updateWeatherValues(){
+        //make some request
+        const fakeResponseFromBackend = {
+            temperature: 5,
+            presure: 5,
+            windSpeed: 5,
+            UVIndex: 5,
+            precipitation: 5
+        }
+        setCurrentTemperature(fakeResponseFromBackend.temperature);
+        setCurrentPresure(fakeResponseFromBackend.presure);
+        setCurrentWindSpeed(fakeResponseFromBackend.windSpeed);
+        setCurrentUVIndex(fakeResponseFromBackend.UVIndex);
+        setCurrentPrecipitation(fakeResponseFromBackend.precipitation);
+    }
 
     return (
         <PageBasicLayout>
@@ -25,6 +45,8 @@ export const LandingPage = () => {
                     <h1>
                         Current city weather preview
                     </h1>
+
+                    <button onClick={updateWeatherValues}>update weather</button>
 
                     <WeatherPreviewCityName>
                         {currentCity}
